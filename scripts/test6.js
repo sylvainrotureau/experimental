9f3772688e161a026f6b4973f2d8eb6398819ffe
sylvain rotureau <syvlainrotureau@yahoo.fr>
2022-06-21 10:19:30 +0200

    fin presque exo6

--- scripts/test6.js
+++ scripts/test6.js
@@ -40,19 +40,100 @@ let team = {
             lastName: "Sanchez",
             age: 11
         },
+    ],
+    _games : [
+        {
+            opponent: "Broncos",
+            teamPoints: 42,
+            opponentPoints: 27
+        },
     ]
-
-       
-
 }
 
 let firstName = "Paul";
-let lastName = "Niort";
-let age = "40";
+let lastName = "MARTIN";
+let age = 40;
+
+let opponent = "Paul";
+let teamPoints = 22;
+let opponentPoints = 40;
+
+let totalGameSum = 0;
+let totalAdvSum = 0;
 
 function addPlayer(firstName, lastName, age) {
-    team._players.push;
+    team._players.push({
+        firstName: firstName,
+        lastName: lastName,
+        age: age
+    });
 }
 
-console.log(team._players[0])
+function addMatch(opponent, teamPoints, opponentPoints) {
+    team._games.push({
+        opponent: opponent,
+        teamPoints: teamPoints,
+        opponentPoints: opponentPoints
+    });
+}
+
+
+function gamePointSum(){
+    for (let i=0; i<team._games.length ; i++){
+        totalGameSum = totalGameSum + team._games[i].teamPoints;
+       
+    }
+}
+
+function gamePointAdvSum(){
+    for (let i=0; i<team._games.length ; i++){
+        totalAdvSum = totalAdvSum + team._games[i].opponentPoints;
+       
+    }
+}
+
+function gamePointAdvAvg(){
+    let avg = totalAdvSum / team._games.length;
+    avg = Math.round(avg);
+    return avg;       
+    
+}
+
+
+function oldest(){
+    let age = 0;
+    let firstName = "";
+    let lastName = "";
+    for (let i=0; i<team._players.length ; i++){
+        if (team._players[i].age >= age) {
+            age = team._players[i].age;
+            firstName = team._players[i].firstName;
+            lastName = team._players[i].lastName;
+        }
+        
+    }
+    return age, firstName, lastName;
+}
+
+for (let i=0;i<10;i++){
+    addPlayer(firstName, lastName, age);
+    addMatch(opponent, teamPoints, opponentPoints);
+}
+
+gamePointSum();
+gamePointAdvSum();
+
+
+console.log(`total des points de notre équipe ${totalGameSum}`);
+console.log(`total des points des équipes adverses ${totalAdvSum}`);
+console.log(`moyenne des points des équipes adverses ${gamePointAdvAvg()}`);
+
+for (let i=0; i<team._players.length ; i++){
+    console.log(`Le joueur ${i} est : ${team._players[i].firstName} ${team._players[i].lastName} ${team._players[i].age}`);
+}
+
+console.log(`Le joueur le plus vieux est ${oldest()}`);
+
+// console.log(team._players);
+// console.log(team._games);
 
